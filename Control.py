dcbe55418e142d43301c72d17e0ef90e14095ea9
Darwin L <lemus@unc.edu>
2024-12-21 20:31:31 -0500

    Some Initial Testing

---
+++ Control.py
@@ -0,0 +1,9 @@
+import pyautogui
+import keyboard
+#print("6")
+#questIconLocation = pyautogui.locateOnScreen("T2.png")
+#print("7")
+#print (questIconLocation)
+#pyautogui.click("MenuB.png")
+#pyautogui.click("InteractButton.png")
+
